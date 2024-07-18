c81066f1bad27b44ec14c13cca5ab5074df8762f
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-07-18 21:03:37 +0100

    remove loader on error

--- src/Center.jsx
+++ src/Center.jsx
@@ -131,7 +131,8 @@ export default function Center() {
       setDload(false);
       setComp(true)
     } catch (e) {
-      setErr(true)
+      setErr(true);
+      setDload(false);
     }
   }
   function checkCanDl(delim){
